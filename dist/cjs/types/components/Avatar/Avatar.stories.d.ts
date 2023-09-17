8134f3ceec8cf717626b63136f3e7d689b281bc7
Afzal Ali <afzalali2728@gmail.com>
2023-09-17 15:01:44 +0500

    build

---
+++ dist/cjs/types/components/Avatar/Avatar.stories.d.ts
@@ -0,0 +1,6 @@
+import { AvatarProps } from './Avatar';
+declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react/dist/types-0a347bb9").R, AvatarProps>;
+export default _default;
+export declare const Test: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, AvatarProps>;
+export declare const HelloWorld: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, AvatarProps>;
+export declare const ReactAvatarInitials: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, AvatarProps>;
